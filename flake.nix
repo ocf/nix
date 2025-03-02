fef3f3e6ef3f2aa5b9f13b1ebe01b8a7e99886db
laksith19 <admin@laksith.dev>
2025-03-01 20:58:08 -0800

    fix: use kde packages for okular

--- flake.nix
+++ flake.nix
@@ -142,7 +142,7 @@
         plasma-applet-commandoutput = final.callPackage ./pkgs/plasma-applet-commandoutput.nix { };
         catppuccin-sddm = final.qt6Packages.callPackage ./pkgs/catppuccin-sddm.nix { };
         ocf-papers = final.callPackage ./pkgs/ocf-papers.nix { };
-        ocf-okular = final.callPackage ./pkgs/ocf-okular.nix { };
+        ocf-okular = final.kdePackages.callPackage ./pkgs/ocf-okular.nix { };
       };
 
       devShells = forAllSystems (pkgs: {
