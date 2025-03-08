4551c5a8ca30f0bbbf4e9853a5fa21997e596103
laksith19 <admin@laksith.dev>
2025-03-08 09:56:55 -0800

    fix: gnome files crash on launch
    remove triple buffering patch
    triple buffering is now merged and part of mutter

--- flake.nix
+++ flake.nix
@@ -50,22 +50,6 @@
         ocf-sync-etc.overlays.default
         ocf-pam-trimspaces.overlays.default
         nix-index-database.overlays.nix-index
-
-        # GNOME 46: triple-buffering-v4-46
-        # See: https://nixos.wiki/wiki/GNOME#Dynamic_triple_buffering
-        (final: prev: {
-          gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
-            mutter = gnomePrev.mutter.overrideAttrs (old: {
-              src = final.fetchFromGitLab {
-                domain = "gitlab.gnome.org";
-                owner = "vanvugt";
-                repo = "mutter";
-                rev = "triple-buffering-v4-46";
-                hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
-              };
-            });
-          });
-        })
       ];
 
       commonModules = [
