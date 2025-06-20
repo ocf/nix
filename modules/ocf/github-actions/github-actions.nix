50c9f49e24084bf8b35c5864f56132e17a0e1e5d
laksith19 <admin@laksith.dev>
2025-06-20 13:13:52 -0700

    fix: move template into the actions module

--- modules/ocf/github-actions/github-actions.nix
+++ modules/ocf/github-actions/github-actions.nix
@@ -2,12 +2,66 @@
 
 let
   cfg = config.ocf.github-actions;
-  template = import ./template.nix;
+  template =
+    { enable, owner, repo, workflow, tokenPath, packages, instances, ... }:
+
+    if enable
+    then
+      {
+        "ci-${owner}-${repo}-${workflow}" = {
+          ephemeral = true;
+          autoStart = true;
+          privateUsers = "pick";
+          bindMounts = {
+            "github-token" = {
+              hostPath = tokenPath;
+              mountPoint = "/run/token";
+              isReadOnly = true;
+            };
+          };
+          config =
+            { ... }:
+            {
+              nix.settings.experimental-features = "nix-command flakes";
+              networking.firewall.enable = true;
+              services.github-runners =
+                builtins.listToAttrs (
+                  builtins.genList
+                    (
+                      i:
+                      let
+                        name = "ci-${owner}-${repo}-${workflow}-${toString (i+1)}";
+                      in
+                      {
+                        name = name;
+                        value = {
+                          enable = true;
+                          ephemeral = true;
+                          user = null;
+                          group = null;
+                          replace = true;
+                          noDefaultLabels = true;
+                          extraLabels = [ "ci-${owner}-${repo}-${workflow}" ];
+                          url = "https://github.com/${owner}/${repo}";
+                          tokenFile = "/run/token";
+                          extraPackages = packages;
+                        };
+                      }
+                    )
+                    instances
+                );
+              system.stateVersion = "24.11";
+            };
+        };
+      }
+    else { };
+
+
 in
 {
 
   imports = [ ./options.nix ];
   config = lib.mkIf cfg.enable {
-    containers = lib.mergeAttrsList (builtins.map (runner: (template (with runner; {inherit enable owner repo workflow tokenPath packages instances;}))) cfg.runners);
+    containers = lib.mergeAttrsList (builtins.map (runner: (template (with runner; { inherit enable owner repo workflow tokenPath packages instances; }))) cfg.runners);
   };
 }
