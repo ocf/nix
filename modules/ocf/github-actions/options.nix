2c40f17c15aa2c2781669ba48a0804ca8d3b1414
laksith19 <admin@laksith.dev>
2025-07-09 13:08:28 -0700

    fmt: run formatter on files

--- modules/ocf/github-actions/options.nix
+++ modules/ocf/github-actions/options.nix
@@ -7,49 +7,49 @@
       type = with types; listOf (submodule {
 
         options = {
-        enable = mkEnableOption "Enable this self-hosted runner";
+          enable = mkEnableOption "Enable this self-hosted runner";
 
-        owner = mkOption {
-          type = str;
-          description = "Owner of the GitHub Repository";
-          default = "ocf";
+          owner = mkOption {
+            type = str;
+            description = "Owner of the GitHub Repository";
+            default = "ocf";
+          };
+
+          repo = mkOption {
+            type = str;
+            description = "Name of the GitHub Repository";
+          };
+
+          workflow = mkOption {
+            type = str;
+            description = "Name of the GitHub Actions Workflow";
+          };
+
+          tokenPath = mkOption {
+            type = path;
+            description = ''
+              Path to GitHub fine grained PAT with the following permissions:
+                Organization:
+                  - Read and Write access to organization self hosted runners
+                Repository:
+                  - Read access to metadata
+                  - Read and Write access to administration
+            '';
+          };
+
+          packages = mkOption {
+            type = listOf package;
+            description = "Packages to be installed in the runner enviornment";
+            default = [ ];
+          };
+
+          instances = mkOption {
+            type = int;
+            description = "Number of parallel instances for this workflow";
+            default = 1;
+          };
+
         };
-
-        repo = mkOption {
-          type = str;
-          description = "Name of the GitHub Repository";
-        };
-
-        workflow = mkOption {
-          type = str;
-          description = "Name of the GitHub Actions Workflow";
-        };
-
-        tokenPath = mkOption {
-          type = path;
-          description = ''
-            Path to GitHub fine grained PAT with the following permissions:
-              Organization:
-                - Read and Write access to organization self hosted runners
-              Repository:
-                - Read access to metadata
-                - Read and Write access to administration
-          '';
-        };
-
-        packages = mkOption {
-          type = listOf package;
-          description = "Packages to be installed in the runner enviornment";
-          default = [ ];
-        };
-
-        instances = mkOption {
-          type =int;
-          description = "Number of parallel instances for this workflow";
-          default = 1;
-        };
-
-      };
       }
       );
     };
