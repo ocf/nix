32b42ba6451bbd2cd912aea6bffd1e25886c4565
laksith19 <admin@laksith.dev>
2025-07-15 03:02:12 -0700

    fmt

--- modules/ocf/github-actions/options.nix
+++ modules/ocf/github-actions/options.nix
@@ -1,32 +1,32 @@
 { lib, ... }:
 
 {
-  options.ocf.github-actions =  {
+  options.ocf.github-actions = {
     enable = lib.mkEnableOption "Enable Containerized OCF GitHub Actions Runners";
     runners = lib.mkOption {
-       type =  lib.types.listOf (lib.types.submodule {
+      type = lib.types.listOf (lib.types.submodule {
 
         options = {
           enable = lib.mkEnableOption "Enable this self-hosted runner";
 
           owner = lib.mkOption {
-             type = lib.types.str;
+            type = lib.types.str;
             description = "Owner of the GitHub Repository";
             default = "ocf";
           };
 
           repo = lib.mkOption {
-             type = lib.types.str;
+            type = lib.types.str;
             description = "Name of the GitHub Repository";
           };
 
           workflow = lib.mkOption {
-             type = lib.types.str;
+            type = lib.types.str;
             description = "Name of the GitHub Actions Workflow";
           };
 
           tokenPath = lib.mkOption {
-             type = lib.types.path;
+            type = lib.types.path;
             description = ''
               Path to GitHub fine grained PAT with the following permissions:
                 Organization:
@@ -38,13 +38,13 @@
           };
 
           packages = lib.mkOption {
-             type = with lib.types; listOf package;
+            type = with lib.types; listOf package;
             description = "Packages to be installed in the runner enviornment";
             default = [ ];
           };
 
           instances = lib.mkOption {
-             type = lib.types.int;
+            type = lib.types.int;
             description = "Number of parallel instances for this workflow";
             default = 1;
           };
