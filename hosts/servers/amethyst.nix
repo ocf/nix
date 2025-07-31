a922bffc11fb4286063554392234e6ba14c66831
jaysa68 <gh@jaysa.net>
2025-07-30 23:30:12 -0700

    webhost module support for multiple websites

--- hosts/servers/amethyst.nix
+++ hosts/servers/amethyst.nix
@@ -12,9 +12,16 @@
 
   ocf.webhost = {
     enable = true;
-    subdomain = "bestdocs";
-    githubActionsPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfbHPz52unvWwGAEVenVycOIQqIoZEj5OYi8vzJ1mJS";
+    websites = [
+      {
+        enable = true;
+        name = "bestdocs";
+        githubActionsPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfbHPz52unvWwGAEVenVycOIQqIoZEj5OYi8vzJ1mJS";
+      }
+    ];
   };
+
+
 
   system.stateVersion = "24.11";
 }
--- modules/ocf/webhost.nix
+++ modules/ocf/webhost.nix
@@ -2,55 +2,87 @@
 
 let
   cfg = config.ocf.webhost;
-in
-{
-  options.ocf.webhost = {
-    enable = lib.mkEnableOption "Enable static webhosting configuration";
-    subdomain = lib.mkOption {
-      type = lib.types.str;
-      description = "Subdomain of webpage - will set <subdomain>.ocf.berkeley.edu & <subdomain>.ocf.io";
-    };
-    githubActionsPubkey = lib.mkOption {
-      type = lib.types.str;
-      description = "SSH Public Key of Github Actions Deploy Workflow";
+
+  makeUsers = website-cfg: lib.mkIf website-cfg.enable {
+    "deploy-${website-cfg.name}" = {
+      group = "nginx";
+      isNormalUser = true;
+      createHome = false;
+      openssh.authorizedKeys.keys = [
+        "${website-cfg.githubActionsPubkey}"
+      ];
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    security.acme.certs."${config.networking.hostName}.ocf.berkeley.edu".group = "nginx";
-    ocf.acme.extraCerts = [ "${cfg.subdomain}.ocf.berkeley.edu" "${cfg.subdomain}.ocf.io" ];
-
-    users.users = {
-      "deploy-${cfg.subdomain}" = {
-        group = "nginx";
-        isNormalUser = true;
-        createHome = false;
-        openssh.authorizedKeys.keys = [
-          "${cfg.githubActionsPubkey}"
-        ];
-      };
+  makeVirtHosts = website-cfg: lib.mkIf website-cfg.enable {
+    "${website-cfg.name}.ocf.berkeley.edu" = {
+      forceSSL = true;
+      useACMEHost = "${config.networking.hostName}.ocf.berkeley.edu";
+      serverAliases = [ "${website-cfg.name}.ocf.io" ];
+      root = "/var/www/${website-cfg.name}";
     };
+  };
 
-    systemd.tmpfiles.rules = [
-      "d /var/www/${cfg.subdomain} 775 deploy-${cfg.subdomain} nginx"
-    ];
-
-    services.nginx = {
-      enable = true;
-      virtualHosts.default-server = {
+  defaultVirtHost =  [
+    {
+      default-server = {
         default = true;
         serverName = "_";
         forceSSL = true;
         useACMEHost = "${config.networking.hostName}.ocf.berkeley.edu";
         locations."/".return = 444;
+      };
+    }
+  ];
 
-      };
-      virtualHosts."${cfg.subdomain}.ocf.berkeley.edu" = {
-        forceSSL = true;
-        useACMEHost = "${config.networking.hostName}.ocf.berkeley.edu";
-        serverAliases = [ "${cfg.subdomain}.ocf.io" ];
-        root = "/var/www/${cfg.subdomain}";
-      };
+  makeDirs = website-cfg:
+    if website-cfg.enable then [ "d /var/www/${website-cfg.name} 775 deploy-${website-cfg.name} nginx" ]
+    else [ ];
+
+
+  makeExtraCerts = website-cfg:
+    if website-cfg.enable then [ "${website-cfg.name}.ocf.berkeley.edu" "${website-cfg.name}.ocf.io" ]
+    else [ ];
+
+in
+{
+  options.ocf.webhost = {
+    enable = lib.mkEnableOption "Enable static webhosting configuration";
+    websites = lib.mkOption {
+      type = lib.types.listOf (lib.types.submodule {
+
+
+        options = {
+          enable = lib.mkEnableOption "Enable this website";
+
+          name = lib.mkOption {
+            type = lib.types.str;
+            description = "Subdomain of webpage - will set <name>.ocf.berkeley.edu & <name>.ocf.io";
+          };
+
+          githubActionsPubkey = lib.mkOption {
+            type = lib.types.str;
+            description = "SSH Public Key of Github Actions Deploy Workflow";
+          };
+        };
+
+
+      }
+      );
+    };
+  };
+  config = lib.mkIf cfg.enable {
+
+    security.acme.certs."${config.networking.hostName}.ocf.berkeley.edu".group = "nginx";
+    users.users = lib.mkMerge (builtins.map makeUsers cfg.websites);
+
+    systemd.tmpfiles.rules = lib.flatten (builtins.map makeDirs cfg.websites);
+    ocf.acme.extraCerts = lib.flatten (builtins.map makeExtraCerts cfg.websites);
+
+
+    services.nginx = {
+      enable = true;
+      virtualHosts = lib.mkMerge ((builtins.map makeVirtHosts cfg.websites) ++ defaultVirtHost);
     };
 
   };
