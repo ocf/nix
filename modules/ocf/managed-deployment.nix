d522244cf431ac7f3912dbaaccada2bb87f09af6
jaysa68 <gh@jaysa.net>
2025-08-04 05:10:43 -0700

    fido2 baby

--- modules/ocf/managed-deployment.nix
+++ modules/ocf/managed-deployment.nix
@@ -17,7 +17,7 @@ in
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMuiOUsjVJSi+0WeMHKquQmwoyz/c3N7HhjJwzz21B3" # github-actions
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlViRB5HH1bTaS1S7TcqVBSuxKdrbdhL2CmhDqc/t6A" # oliverni
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILOaJJvOUG08qr3yeeQRB71M30cdPMuO69nsf0CodALa" # jaysa
-	"sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFQzoOCl3/H1FmfKsJJ72gRq57zf0sV/noJz+rqJeKcRAAAAEXNzaDpqYXlzYS15dWJpa2V5" # jaysa hardware token
+	"sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINYJKGY4Q1oSfed6ER50bCXlGaj1RSAwnxLMBOyzRJo5AAAABHNzaDo=" #jaysa hardware token
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHPeJeRNwcPaZupbmCEtUIOuLDfhow35byMp548TUDYP" # rjz
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO6zftyMUeIQVYkRag6CxWqYShjWnErQ24NeaU95Bp2z" # laksith
 	"sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIGU0k2swUbmqWcAoOjG64ekaahK05iyRPPQqlsgjp32fAAAABHNzaDo=" # laksith hardware token
