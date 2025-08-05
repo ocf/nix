e37b1110d60900a8d61b31dcb4c94e856f4b21c4
jaysa68 <gh@jaysa.net>
2025-08-05 02:37:42 -0700

    lol syntax

--- hosts/servers/zecora.nix
+++ hosts/servers/zecora.nix
@@ -15,27 +15,31 @@
     settings = {
       network.name = "OCF";
       server = {
-         name = "dev-irc.ocf.berkeley.edu";
-	 sts.enabled = true;
+        name = "dev-irc.ocf.berkeley.edu";
+        sts.enabled = true;
+	listeners.":6697".tls = {
+	  cert = "/home/ergochat/fullchain.pem";
+	  key = "/home/ergochat/privkey.pem";
+	};
       };
     };
   };
 
   ocf.acme.extraCerts = [ "dev-irc.ocf.berkeley.edu" "dev-irc.ocf.io" ];
 
-  security.acme.certs."dev-irc.ocf.berkeley.edu".group = "ergochat";
-  security.acme.certs."dev-irc.ocf.berkeley.edu".user = "ergochat";
-  users.users."ergo" {
+  users.users."ergochat" = {
+    isNormalUser = true;
     createHome = true;
+    group = "acme";
   };
 
-  system.ActivationScripts = {
+  system.activationScripts = {
     link-ergochat-certs = {
-    text =
-      ''
-        ln -sfn /var/lib/acme/zecora.ocf.berkeley.edu/fullchain.pem /home/ergo/fullchain.pem
-        ln -sfn /var/lib/acme/zecora.ocf.berkeley.edu/key.pem /home/ergo/privkey.pem
-      '';
+      text =
+        ''
+          ln -sfn /var/lib/acme/zecora.ocf.berkeley.edu/fullchain.pem /home/ergochat/fullchain.pem
+          ln -sfn /var/lib/acme/zecora.ocf.berkeley.edu/key.pem /home/ergochat/privkey.pem
+        '';
     };
   };
 
