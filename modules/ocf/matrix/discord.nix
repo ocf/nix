0b4c55f3674f1c5ad4c7baf38e61509d9fd76d74
sophiebsw <sophiebsw@gmail.com>
2025-08-09 01:57:11 -0700

    add matrix discord bridge

---
+++ modules/ocf/matrix/discord.nix
@@ -0,0 +1,40 @@
+{ pkgs, lib, config, ... }:
+
+let 
+  cfg = config.ocf.matrix;
+in
+{
+  options.ocf.matrix.discord = {
+    enable = lib.mkEnableOption "Enable Matrix Discord bridge.";
+    
+    baseUrl = lib.mkOption {
+      type = lib.types.str;
+      description = "Synapse base URL.";
+    };
+
+    serverName = lib.mkOption {
+      type = lib.types.str;
+      description = "Synapse server name.";
+    };
+  };
+
+  config = lib.mkIf cfg.discord.enable {
+    age.secrets.discord-bot-token.rekeyFile = ../../secrets/master-keyed/matrix/bot-token.age;
+
+    services.matrix-appservice-discord = {
+      bridge = {
+        domain = cfg.serverName;
+        homeserverUrl = cfg.baseUrl;
+
+        enableSelfServiceBridging = true;
+      };
+
+      auth = {
+        clientID = "1403658564685402203";
+        # this is an anti-pattern, but i'm not sure there's an alternative
+        botToken = builtins.readFile config.age.secrets.discord-bot-token.path;
+        usePrivilegedIntents = true;
+      };
+    };
+  };
+}
