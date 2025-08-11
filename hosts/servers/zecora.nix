650bb380825359a25af2d707be2f261aaecd60df
jaysa68 <gh@jaysa.net>
2025-08-11 16:31:28 -0700

    init motd

--- hosts/servers/zecora.nix
+++ hosts/servers/zecora.nix
@@ -5,7 +5,12 @@
 
   networking.hostName = "zecora";
   # TODO: Move IRC related config into a custom OCF module
-  
+
+  ocf.network = {
+    enable = true;
+    lastOctet = 44;
+  };
+
   age.secrets.irc-passwd.rekeyFile = ../../secrets/master-keyed/irc-passwd.age;
 
   security.acme.defaults.reloadServices = [ "ergochat.service" ];
@@ -16,51 +21,47 @@
     ${lib.getExe pkgs.gnused} -i "s/@irc-passwd@/$secret/g" "$configFile"
   '';
 
-  ocf.network = {
-    enable = true;
-    lastOctet = 44;
-  };
-
   services.ergochat = {
     enable = true;
     settings = {
-    oper-classes = {
-      "server-admin" = {
-        title = "Server Admin";
-        "capabilities" = [
-          "kill"      # disconnect user sessions
-          "ban"   # ban IPs, CIDRs, NUH masks, and suspend accounts (UBAN / DLINE / KLINE)
-          "nofakelag" # exempted from fakelag restrictions on rate of message sending
-          "relaymsg" # use RELAYMSG in any channel (see the `relaymsg` config block)
-          "vhosts" # add and remove vhosts from users
-          "sajoin" # join arbitrary channels, including private channels
-          "samode" # modify arbitrary channel and user modes
-          "snomasks" # subscribe to arbitrary server notice masks
-          "roleplay" # use the (deprecated) roleplay commands in any channel
-          "rehash"   # rehash the server, i.e. reload the config at runtime
-          "accreg"  # modify arbitrary account registrations
-          "chanreg" # modify arbitrary channel registrations
-          "history" # modify or delete history messages
-          "defcon" # use the DEFCON command (restrict server capabilities)
-          "massmessage" # message all users on the server
-          "metadata" # modify arbitrary metadata on channels and users
-        ];
+      oper-classes = {
+        "server-admin" = {
+          title = "Server Admin";
+          "capabilities" = [
+            "kill" # disconnect user sessions
+            "ban" # ban IPs, CIDRs, NUH masks, and suspend accounts (UBAN / DLINE / KLINE)
+            "nofakelag" # exempted from fakelag restrictions on rate of message sending
+            "relaymsg" # use RELAYMSG in any channel (see the `relaymsg` config block)
+            "vhosts" # add and remove vhosts from users
+            "sajoin" # join arbitrary channels, including private channels
+            "samode" # modify arbitrary channel and user modes
+            "snomasks" # subscribe to arbitrary server notice masks
+            "roleplay" # use the (deprecated) roleplay commands in any channel
+            "rehash" # rehash the server, i.e. reload the config at runtime
+            "accreg" # modify arbitrary account registrations
+            "chanreg" # modify arbitrary channel registrations
+            "history" # modify or delete history messages
+            "defcon" # use the DEFCON command (restrict server capabilities)
+            "massmessage" # message all users on the server
+            "metadata" # modify arbitrary metadata on channels and users
+          ];
+        };
       };
-    };
       opers = {
         admin = {
-	  class = "server-admin";
-	  password = "@irc-passwd@";
-	};
+          class = "server-admin";
+          password = "@irc-passwd@";
+        };
       };
       network.name = "OCF";
       server = {
         name = "irc.ocf.berkeley.edu";
+        motd = pkgs.writeText "ircd.motd" "hiiiii";
         sts.enabled = true;
-	listeners.":6697".tls = {
-	  cert = "/var/lib/acme/zecora.ocf.berkeley.edu/fullchain.pem";
-	  key = "/var/lib/acme/zecora.ocf.berkeley.edu/key.pem";
-	};
+        listeners.":6697".tls = {
+          cert = "/var/lib/acme/zecora.ocf.berkeley.edu/fullchain.pem";
+          key = "/var/lib/acme/zecora.ocf.berkeley.edu/key.pem";
+        };
       };
     };
   };
