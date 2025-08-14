9a7a4c11ec4f4a5b1e76b0100748df6825be7ace
jaysa68 <gh@jaysa.net>
2025-08-14 01:21:06 -0700

    make irc module, remove config from host

--- hosts/servers/zecora.nix
+++ hosts/servers/zecora.nix
@@ -11,81 +11,8 @@
     lastOctet = 44;
   };
 
-  age.secrets.irc-passwd.rekeyFile = ../../secrets/master-keyed/irc-passwd.age;
-
-  security.acme.defaults.reloadServices = [ "ergochat.service" ];
-
-  system.activationScripts."irc-passwd" = ''
-    secret=$(cat "${config.age.secrets.irc-passwd.path}")
-    configFile=/etc/ergo.yaml
-    ${lib.getExe pkgs.gnused} -i "s/@irc-passwd@/$secret/g" "$configFile"
-  '';
-
-  services.ergochat = {
+  ocf.irc = {
     enable = true;
-    settings = {
-      channels = {
-        operator-only-creation = true;
-	auto-join = [
-	  "#announcements"
-	  "#introduce-yourself"
-	  "#rebuild"
-	  "#off-topic"
-	  "#board-games"
-	  "#hack-day"
-	  "#decal"
-	  "#administrivia"
-	  "#opstaff"
-	];
-      };
-      oper-classes = {
-        "server-admin" = {
-          title = "Server Admin";
-          "capabilities" = [
-            "kill" # disconnect user sessions
-            "ban" # ban IPs, CIDRs, NUH masks, and suspend accounts (UBAN / DLINE / KLINE)
-            "nofakelag" # exempted from fakelag restrictions on rate of message sending
-            "relaymsg" # use RELAYMSG in any channel (see the `relaymsg` config block)
-            "vhosts" # add and remove vhosts from users
-            "sajoin" # join arbitrary channels, including private channels
-            "samode" # modify arbitrary channel and user modes
-            "snomasks" # subscribe to arbitrary server notice masks
-            "roleplay" # use the (deprecated) roleplay commands in any channel
-            "rehash" # rehash the server, i.e. reload the config at runtime
-            "accreg" # modify arbitrary account registrations
-            "chanreg" # modify arbitrary channel registrations
-            "history" # modify or delete history messages
-            "defcon" # use the DEFCON command (restrict server capabilities)
-            "massmessage" # message all users on the server
-            "metadata" # modify arbitrary metadata on channels and users
-          ];
-        };
-      };
-      opers = {
-        admin = {
-          class = "server-admin";
-          password = "@irc-passwd@";
-        };
-      };
-      network.name = "OCF";
-      server = {
-        name = "irc.ocf.berkeley.edu";
-        motd = pkgs.writeText "ircd.motd" "hiiiii";
-        sts.enabled = true;
-        listeners.":6697".tls = {
-          cert = "/var/lib/acme/zecora.ocf.berkeley.edu/fullchain.pem";
-          key = "/var/lib/acme/zecora.ocf.berkeley.edu/key.pem";
-        };
-      };
-    };
-  };
-
-  ocf.acme.extraCerts = [ "irc.ocf.berkeley.edu" "irc.ocf.io" ];
-
-  users.users."ergochat" = {
-    isNormalUser = true;
-    createHome = true;
-    group = "acme";
   };
 
   system.stateVersion = "24.11";
