6c30488d6ceb779924dbb062bd54490656850534
jaysa68 <gh@jaysa.net>
2025-09-06 23:06:46 -0700

    comments identifying initial rooms

--- hosts/servers/scootaloo.nix
+++ hosts/servers/scootaloo.nix
@@ -23,20 +23,22 @@
     irc-bridge.enable = true;
 
     initialRooms = [
-      "#_discord_735620315111096391_761065162450272286:ocf.io"
-      "#_discord_735620315111096391_735624339847643277:ocf.io"
-      "#_discord_735620315111096391_736066924592758785:ocf.io"
-      "#_discord_735620315111096391_833090052996857876:ocf.io"
-      "#_discord_735620315111096391_1012214233322246214:ocf.io"
-      "#_discord_735620315111096391_735624292179640331:ocf.io"
-      "#_discord_735620315111096391_735624203860181135:ocf.io"
-      "#_discord_735620315111096391_881325795044368414:ocf.io"
-      "#_discord_735620315111096391_736005105068802109:ocf.io"
-      "#_discord_735620315111096391_1288710167633985536:ocf.io"
+      # right click channel in discord, "Copy Channel ID"
+      "#_discord_735620315111096391_761065162450272286:ocf.io" # opstaff
+      "#_discord_735620315111096391_735624339847643277:ocf.io" # administrivia
+      "#_discord_735620315111096391_736066924592758785:ocf.io" # decal
+      "#_discord_735620315111096391_833090052996857876:ocf.io" # hack-day
+      "#_discord_735620315111096391_1012214233322246214:ocf.io" # board-games
+      "#_discord_735620315111096391_735624292179640331:ocf.io" # off-topic
+      "#_discord_735620315111096391_735624203860181135:ocf.io" # rebuild
+      "#_discord_735620315111096391_881325795044368414:ocf.io" # introduce-yourself
+      "#_discord_735620315111096391_736005105068802109:ocf.io" # announcements
+      "#_discord_735620315111096391_1288710167633985536:ocf.io" # design
     ];
 
     irc-bridge.initialRooms = {
-      "#announcements".roomIds = [ "!cEgfyfEHPMQGtNliqC:ocf.io" ];
+      # room ID: log onto matrix, right-click channel, click settings, click Advanced, should see "Internal room ID" section
+      "#announcements".roomIds = [ "!aOxFvQVvHZsjmmZFaG:ocf.io" ];
       "#introduce-yourself".roomIds = [ "!TLkoqDXRVMyWEMXHva:ocf.io" ];
       "#rebuild".roomIds = [ "!cEgfyfEHPMQGtNliqC:ocf.io" ];
       "#off-topic".roomIds = [ "!zssQqFOjeDgMvEitnT:ocf.io" ];
