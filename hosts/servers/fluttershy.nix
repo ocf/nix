55a85915fb1aa5bbf8e5a93157c3c789f20358cd
sophiebsw <sophiebsw@gmail.com>
2025-10-07 01:21:58 -0700

    add nfs mounts to fluttershy

--- hosts/servers/fluttershy.nix
+++ hosts/servers/fluttershy.nix
@@ -15,5 +15,11 @@
   # staff-only while wip
   #ocf.auth.extra_access_conf = [ "+:(ocf):ALL" "+:(sorry):ALL" ];
 
+  ocf.nfs = {
+    enable = true;
+    mountHome = true;
+    mountServices = true;
+  };
+
   system.stateVersion = "25.05";
 }
