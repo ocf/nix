8f68d60feeef559ec819ccec4fb1d8815e5bcb1c
sophiebsw <sophiebsw@gmail.com>
2025-10-07 20:14:46 -0700

    configure ldap.conf

--- modules/ocf/auth.nix
+++ modules/ocf/auth.nix
@@ -40,16 +40,18 @@ in
         server = "ldaps://ldap.ocf.berkeley.edu";
         base = "dc=OCF,dc=Berkeley,dc=EDU";
         daemon.enable = true;
-        extraConfig = ''
-          tls_reqcert hard
-          tls_cacert /etc/ssl/certs/ca-certificates.crt
-
-          base dc=ocf,dc=berkeley,dc=edu
-          nss_base_passwd ou=people,dc=ocf,dc=berkeley,dc=edu
-          nss_base_group  ou=group,dc=ocf,dc=berkeley,dc=edu
-        '';
       };
     };
+
+    environment.etc."ldap.conf".text = 
+      ''
+        tls_reqcert hard
+        tls_cacert /etc/ssl/certs/ca-certificates.crt
+
+        base dc=ocf,dc=berkeley,dc=edu
+        nss_base_passwd ou=people,dc=ocf,dc=berkeley,dc=edu
+        nss_base_group  ou=group,dc=ocf,dc=berkeley,dc=edu
+      '';
 
     security.sudo = {
       extraConfig = ''
