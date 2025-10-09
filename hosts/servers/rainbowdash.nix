9c56e898c6a3621045e64c0cb31beb138f64de13
Joe Wang <zezhou_wang@berkeley.edu>
2025-10-09 14:28:41 -0700

    feat: rainbowdash

---
+++ hosts/servers/rainbowdash.nix
@@ -0,0 +1,23 @@
+{ pkgs, lib, config, ... }:
+
+{
+  imports = [ ../../hardware/virtualized.nix ];
+
+  networking.hostName = "rainbowdash";
+
+  ocf.network = {
+    enable = true;
+    lastOctet = 129;
+  };
+
+  ocf.nfs = {
+    enable = true;
+    mountHome = true;
+    mountServices = true;
+  };
+  
+  environtment.systemPackages = with pkgs; [
+    ipmitool
+  ]
+  system.stateVersion = "25.05";
+}
