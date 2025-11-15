9c860784a589aa231b9f949049678dd447786b51
JimanQAQ <69059495+JimanQAQ@users.noreply.github.com>
2025-11-15 10:44:51 -0800

    big game wallpaper

--- modules/ocf/graphical/default.nix
+++ modules/ocf/graphical/default.nix
@@ -78,7 +78,7 @@ in
       (catppuccin-sddm.override {
         themeConfig.General = {
           FontSize = 12;
-          Background = "/etc/ocf-assets/images/login.png";
+          Background = "/etc/ocf-assets/images/login-winter.png.png";
           #Logo = "/etc/ocf-assets/images/penguin.svg";
           CustomBackground = true;
         };
