81378ede3ae688df10ff567e102d60d7d99011ee
JimanQAQ <69059495+JimanQAQ@users.noreply.github.com>
2026-02-01 22:00:26 -0800

    change spring wallpaper (#99)
    * Add files via upload
    
    * big game wallpaper
    
    * update the april fool wallpaper
    
    * Rename aprilFoolWallpaper2025.png to afool-login.png
    
    * Update and rename afool-login.png to login-aprilfool.png
    
    * Delete modules/ocf/graphical/assets/images/login-afool.png
    
    * Update and rename login-aprilfool.png to login-afool.png
    
    * Update default.nix
    
    * Rename login.png to login-reg.png
    
    * Rename login-newyear.png to login.png
    
    ---------
    
    Co-authored-by: Jaysa Maria Garcia <114126812+jaysa68@users.noreply.github.com>

--- modules/ocf/graphical/default.nix
+++ modules/ocf/graphical/default.nix
@@ -82,7 +82,7 @@ in
       (catppuccin-sddm.override {
         themeConfig.General = {
           FontSize = 12;
-          Background = "/etc/ocf-assets/images/login.png";
+          Background = "/etc/ocf-assets/images/login-winter.png";
           #Logo = "/etc/ocf-assets/images/penguin.svg";
           CustomBackground = true;
         };
