665bbf4eda2bb06aa6599a41a4c4d050f539f6fe
24apricots <michaelzls@berkeley.edu>
2026-02-10 19:36:43 -0800

    reorganize graphical and desktop configs
    - profile/desktop.nix: minimal config for desktops
    - modules/ocf/graphical/default.nix: desktop environment or compositor config
    - modules/ocf/graphical/preinstall-apps.nix: extra preinstalled desktop apps

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -1,3 +1,5 @@
+# basic minimal profile for desktops
+
 { pkgs, lib, inputs, ... }:
 
 # TODO: Move this to pkgs/ or come up with a better way to manage custom scripts
@@ -9,6 +11,16 @@ let
   '';
   # override ocf-tv from util
   ocf-tv = lib.hiPrio vncScript;
+
+  # Default openssh doesn't include GSSAPI support, so we need to override sshfs
+  # to use the openssh_gssapi package instead. This is annoying because the
+  # sshfs package's openssh argument is nested in another layer of callPackage,
+  # so we override callPackage instead to override openssh.
+  sshfs = pkgs.sshfs.override {
+    callPackage = fn: args: (pkgs.callPackage fn args).override {
+      openssh = pkgs.openssh_gssapi;
+    };
+  };
 in
 {
 
@@ -21,86 +33,47 @@ in
 
     etc.enable = true;
     graphical.enable = true;
+    graphical.install-extra-apps = true;
     browsers.enable = true;
     tmpfsHome.enable = true;
     network.wakeOnLan.enable = true;
   };
 
-  boot.loader.systemd-boot.consoleMode = "max";
+  boot = {
+    loader.systemd-boot.consoleMode = "max";
+    loader.timeout = 0;
+    initrd.systemd.enable = true;
+  };
 
   # Enable support SANE scanners
   hardware.sane.enable = true;
 
   documentation.dev.enable = true;
 
+  security.pam = {
+    # Mount ~/remote
+    services.login.pamMount = true;
+    services.login.rules.session.mount.order = config.security.pam.services.login.rules.session.krb5.order + 50;
+    mount.extraVolumes = [ ''<volume fstype="fuse" path="${lib.getExe sshfs}#%(USER)@tsunami:" mountpoint="~/remote/" options="follow_symlinks,UserKnownHostsFile=/dev/null,StrictHostKeyChecking=no" pgrp="ocf" />'' ];
+
+    # Trim spaces from username
+    services.login.rules.auth.trimspaces = {
+      control = "requisite";
+      modulePath = "${pkgs.ocf-pam_trimspaces}/lib/security/pam_trimspaces.so";
+      order = 0;
+    };
+
+    # This contains a bunch of KDE, etc. configs
+    makeHomeDir.skelDirectory = "/etc/skel";
+  };
+
   environment.systemPackages = with pkgs; [
-    # Editors
-    emacs
-    neovim
-    helix
-    kakoune
-
-    # Languages
-    (python3.withPackages (ps: [ ps.tkinter ]))
-    poetry
-    ruby
-    elixir
-    clojure
-    ghc
-    rustup
-    clang
-    nodejs_22
-    graphviz
-    nix-du
-    nix-output-monitor
-    dix
-    lldb
-    gdb
-    valgrind
-    go
-    sqlite
-    zulu25
-
-    # File management tools
-    zip
-    unzip
-    _7zz
-    eza
-    lsd
-    tree
-    bat
-    ranger
     lf
-    fd
     dua
-    rclone
-
-    # Other tools
-    bar
-    fzf
+    tree
     tmux
-    s-tui
-    fio
+
     ocf-tv
-    remmina
-    simple-scan
-    cdrtools # useful for iso files even without a cd drive
-    wiremix
-    yubikey-manager
-    gh
-
-    # Cosmetics
-    neofetch
-    pfetch-rs
-
-    # devtools
-    devenv
-    claude-code
-
-    fastfetch
-    onefetch
-    cpufetch
-    gpufetch
   ];
 
 
