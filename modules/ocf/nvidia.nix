2fd2fc424fad20b6a034b27b56eb131ab5591de7
24apricots <michaelzls@berkeley.edu>
2026-02-17 02:33:28 -0800

    fix suspend on nvidia hosts

--- modules/ocf/nvidia.nix
+++ modules/ocf/nvidia.nix
@@ -19,5 +19,18 @@ in
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia.modesetting.enable = true;
     hardware.nvidia.open = cfg.open;
+
+    # userspace nvidia drivers to restore video memory
+    hardware.nvidia.powerManagement.enable = true;
+    boot.extraModprobeConfig = ''
+      # needed to properly restore video memory after leaving suspend
+      options nvidia NVreg_PreserveVideoMemoryAllocations=1
+
+      # nvidia upstream defaults to /tmp, but recommends not storing on tmpfs.
+      # our desktops have 32 gb of memory so this should be fine, and i would
+      # prefer if we avoided storing video memory on unencrypted filesystems.
+      # this is further improved if zram or encrypted swap is used.
+      options nvidia NVreg_TemporaryFilePath=/tmp
+    '';
   };
 }
