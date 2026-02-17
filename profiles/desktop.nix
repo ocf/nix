ed0fe34d2103ab3cccbdaa00c31c4278cd658950
24apricots <michaelzls@berkeley.edu>
2026-02-16 22:11:22 -0800

    tmpfs for each home, unmounted on logout
    - tmpfs will be mounted on /home and each users home directory, and
      unmounted on logout so that data is cleared immediately instead of on
      reboot.
    - stuff related to home directories moved to an ocf nix module

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -11,16 +11,6 @@ let
   '';
   # override ocf-tv from util
   ocf-tv = lib.hiPrio vncScript;
-
-  # Default openssh doesn't include GSSAPI support, so we need to override sshfs
-  # to use the openssh_gssapi package instead. This is annoying because the
-  # sshfs package's openssh argument is nested in another layer of callPackage,
-  # so we override callPackage instead to override openssh.
-  sshfs = pkgs.sshfs.override {
-    callPackage = fn: args: (pkgs.callPackage fn args).override {
-      openssh = pkgs.openssh_gssapi;
-    };
-  };
 in
 {
 
@@ -35,7 +25,7 @@ in
     graphical.enable = true;
     graphical.install-extra-apps = true;
     browsers.enable = true;
-    tmpfsHome.enable = true;
+    home.tmpfs = true;
     network.wakeOnLan.enable = true;
   };
 
@@ -49,23 +39,6 @@ in
   hardware.sane.enable = true;
 
   documentation.dev.enable = true;
-
-  security.pam = {
-    # Mount ~/remote
-    services.login.pamMount = true;
-    services.login.rules.session.mount.order = config.security.pam.services.login.rules.session.krb5.order + 50;
-    mount.extraVolumes = [ ''<volume fstype="fuse" path="${lib.getExe sshfs}#%(USER)@tsunami:" mountpoint="~/remote/" options="follow_symlinks,UserKnownHostsFile=/dev/null,StrictHostKeyChecking=no" pgrp="ocf" />'' ];
-
-    # Trim spaces from username
-    services.login.rules.auth.trimspaces = {
-      control = "requisite";
-      modulePath = "${pkgs.ocf-pam_trimspaces}/lib/security/pam_trimspaces.so";
-      order = 0;
-    };
-
-    # This contains a bunch of KDE, etc. configs
-    makeHomeDir.skelDirectory = "/etc/skel";
-  };
 
   environment.systemPackages = with pkgs; [
     lf
