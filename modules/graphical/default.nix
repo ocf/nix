5024f6cfe619f3d6abc6ba96da97a5e7d8a8e24b
Daniel Liu <danielliu@berkeley.edu>
2026-04-02 16:48:43 -0700

    revert qt5ct env variable

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -163,7 +163,7 @@ in
         ];
 
         environment.sessionVariables = {
-          QT_QPA_PLATFORMTHEME = "gtk3";
+          QT_QPA_PLATFORMTHEME = "qt5ct";
         };
 
         programs.dconf.enable = true;
