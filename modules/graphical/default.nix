426f420ea9ade052a0b25ca8798c4f92a7898d62
Blake Haug <blake@blakehaug.com>
2026-04-02 17:32:25 -0700

    force gsettings update on theme change

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -232,10 +232,12 @@ in
                 if [ "$content" = "true" ]; then
                   echo "dark" > "$OCF_THEME_FILE"
                   sed -i -E 's/bg-(light|dark)/bg-dark/g' $COSMIC_BG_FILE
+                  gsettings set org.gnome.desktop.interface color-scheme prefer-dark
                   sed -i 's/theme = "rose-pine-dawn"/theme = "rose-pine"/' $HOME/.config/halloy/config.toml
                 else
                   echo "light" > "$OCF_THEME_FILE"
                   sed -i -E 's/bg-(light|dark)/bg-light/g' $COSMIC_BG_FILE
+                  gsettings set org.gnome.desktop.interface color-scheme prefer-light
                   sed -i 's/theme = "rose-pine"/theme = "rose-pine-dawn"/' $HOME/.config/halloy/config.toml
                 fi
                 pkill -USR1 halloy || true
