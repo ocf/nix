5b55c1bf6bdad10759a7d71ab1751a1cd3afd7ff
Blake Haug <blake@blakehaug.com>
2026-04-09 22:38:58 -0700

    use everywhere filters on 1st stage and ppd on 3rd

--- modules/printhost/cups.nix
+++ modules/printhost/cups.nix
@@ -128,25 +128,25 @@ in
         done
 
         lpadmin -p logjam \
-          -v socket://169.229.226.92:9100 \
+          -v ocfbackend:socket://169.229.226.92:9100 \
           -P ${hpPpd} \
           -D "HP LaserJet M806" -L "OCF lab" \
           -E -o printer-is-shared=false -o Duplex=DuplexNoTumble
 
         lpadmin -p pagefault \
-          -v socket://169.229.226.91:9100 \
+          -v ocfbackend:socket://169.229.226.91:9100 \
           -P ${hpPpd} \
           -D "HP LaserJet M806" -L "OCF lab" \
           -E -o printer-is-shared=false -o Duplex=DuplexNoTumble
 
         lpadmin -p papercut \
-          -v socket://169.229.226.93:9100 \
+          -v ocfbackend:socket://169.229.226.93:9100 \
           -P ${hpPpd} \
           -D "HP LaserJet M806" -L "OCF lab" \
           -E -o printer-is-shared=false -o Duplex=DuplexNoTumble
 
         lpadmin -p epson \
-          -v ipps://169.229.226.96/ipp/print \
+          -v ocfbackend:ipps://169.229.226.96/ipp/print \
           -P ${epsonPpd} \
           -D "Epson ET-5880 Color" -L "OCF lab" \
           -E -o printer-is-shared=false -o Duplex=DuplexNoTumble -o PageSize=Letter
@@ -163,13 +163,13 @@ in
 
         # ── Public Printers -------------─────────────────────────────────────
         lpadmin -p OCF-BW \
-          -v ocfbackend:ipp://localhost/classes/OCF-BW-Group \
-          -P ${hpPpd} \
+          -v ipp://localhost/classes/OCF-BW-Group \
+          -m everywhere \
           -D "OCF Black & White" -L "OCF lab" \
           -E -o printer-is-shared=true -o Duplex=DuplexNoTumble
         lpadmin -p OCF-Color \
-          -v ocfbackend:ipp://localhost/classes/OCF-Color-Group \
-          -P ${epsonPpd} \
+          -v ipp://localhost/classes/OCF-Color-Group \
+          -m everywhere \
           -D "OCF Color" -L "OCF lab" \
           -E -o printer-is-shared=true -o Duplex=DuplexNoTumble -o PageSize=Letter
       '';
