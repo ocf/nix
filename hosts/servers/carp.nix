77ee5419df68dcf6e744fe13e4ba27eb3c23cfae
jaysa68 <jaysagarcia@berkeley.edu>
2026-04-10 02:35:55 -0700

    make it soft

--- hosts/servers/carp.nix
+++ hosts/servers/carp.nix
@@ -31,13 +31,13 @@
   ];
 
   security.pam.loginLimits = [
-    { domain = "*"; type = "soft"; item = "cpu";        value = "3600"; }
+    { domain = "*"; type = "-";    item = "cpu";        value = "3600"; }
     { domain = "*"; type = "soft"; item = "stack";      value = "4096"; }
     { domain = "*"; type = "soft"; item = "core";       value = "0"; }
     { domain = "*"; type = "soft"; item = "nproc";      value = "250"; }
     { domain = "*"; type = "soft"; item = "nofile";     value = "1024"; }
-    { domain = "*"; type = "soft"; item = "memlock";    value = "2047219"; }
-    { domain = "*"; type = "soft"; item = "as";         value = "12000000"; }
+    { domain = "*"; type = "-";    item = "memlock";    value = "2047219"; }
+    { domain = "*"; type = "-";    item = "as";         value = "12000000"; }
     { domain = "*"; type = "soft"; item = "sigpending"; value = "63810"; }
     { domain = "*"; type = "soft"; item = "msgqueue";   value = "819200"; }
     { domain = "*"; type = "soft"; item = "nice";       value = "0"; }
