a6a704a80c94bfa8142e472024a23607a8d24a15
jaysa68 <jaysagarcia@berkeley.edu>
2026-04-10 02:33:34 -0700

    security.pam.loginLimits for carp

--- hosts/servers/carp.nix
+++ hosts/servers/carp.nix
@@ -30,5 +30,19 @@
     ipmitool
   ];
 
+  security.pam.loginLimits = [
+    { domain = "*"; type = "soft"; item = "cpu";        value = "3600"; }
+    { domain = "*"; type = "soft"; item = "stack";      value = "4096"; }
+    { domain = "*"; type = "soft"; item = "core";       value = "0"; }
+    { domain = "*"; type = "soft"; item = "nproc";      value = "250"; }
+    { domain = "*"; type = "soft"; item = "nofile";     value = "1024"; }
+    { domain = "*"; type = "soft"; item = "memlock";    value = "2047219"; }
+    { domain = "*"; type = "soft"; item = "as";         value = "12000000"; }
+    { domain = "*"; type = "soft"; item = "sigpending"; value = "63810"; }
+    { domain = "*"; type = "soft"; item = "msgqueue";   value = "819200"; }
+    { domain = "*"; type = "soft"; item = "nice";       value = "0"; }
+    { domain = "*"; type = "soft"; item = "rtprio";     value = "0"; }
+  ];
+
   system.stateVersion = "25.05";
 }
