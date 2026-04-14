60d933b6e1c169462e35ae09ee0004716a26fd3a
Daniel Liu <danxliu@protonmail.com>
2026-04-13 20:12:35 -0700

    fix: another incorrect rebase

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -119,7 +119,7 @@ in
   # Add forward flag to tickets on desktops
   security.krb5.settings.libdefaults.forwardable = true;
 
-  # Only forward Kerberos tickets to login servers (fluttershy and rainbowdash)
+  # Only forward Kerberos tickets to login servers (carp and koi)
   programs.ssh.extraConfig = lib.mkOverride 90 ''
     CanonicalizeHostname yes
     CanonicalDomains ocf.berkeley.edu
