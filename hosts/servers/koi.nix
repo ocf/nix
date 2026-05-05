a9e1d5909efd888597743f25be29b93c7f298dde
jaysa <gh@jaysa.net>
2026-05-05 06:31:10 -0700

    add secrets, need to rekey locally

--- hosts/servers/koi.nix
+++ hosts/servers/koi.nix
@@ -40,5 +40,47 @@
     mode = "0640";
   };
 
+  age.secrets.ocfweb-conf = {
+    rekeyFile = ../../secrets/master-keyed/koi/ocfweb.conf.age;
+    path = "/etc/ocfweb/ocfweb.conf";
+    owner = "root";
+    mode = "0644";
+  };
+
+  age.secrets.puppet-ca = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-ca.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-ca.pem";
+    owner = "root";
+    mode = "0644";
+  };
+
+  age.secrets.puppet-cert = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-cert.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-cert.pem";
+    owner = "root";
+    mode = "0644";
+  };
+
+  age.secrets.puppet-private = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-private.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-private.pem";
+    owner = "root";
+    mode = "0600";
+  };
+
+  age.secrets.puppet-public = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-public.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-public.pem";
+    owner = "root";
+    mode = "0644";
+  };
+
+  age.secrets.puppet-signed = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-signed.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-signed.pem";
+    owner = "root";
+    mode = "0644";
+  };
+
   system.stateVersion = "25.05";
 }
