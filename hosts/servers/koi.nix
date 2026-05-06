0188ed3b0539c4c0c8c7459c57e120fe8162cf86
Jaysa Maria Garcia <114126812+jaysa68@users.noreply.github.com>
2026-05-06 04:08:07 -0700

    Add ocfweb dev conf (#312)
    * Update ocflib flake input
    
    The only module in the nix repo which uses this library at the moment is
    printhost, and the only host using that module is `tule`. There are some
    changes made to printers.py in [the corresponding commit on ocflib](https://github.com/ocf/ocflib/commit/94d13fc75cdf9c828420cbd876c6e868915ae307), the most relevant being that `pysnmp` was replaced with `puresnmp`. Hopefully, as other services are migrated to nix hosts or kubernetes, they will also use this updated version of ocflib.
    
    * add secrets, need to rekey locally
    
    * rekey secrets
    
    * add comments explaining ocfweb-conf and puppet cert secrets, tweak motd
    
    * update after updating deps in default.nix of ocflib
    
    * fixed default.nix, we can track master again for ocflib
    
    * make koi ocfstaff, not root as owner. change perms
    
    * puppet-private-pem 0644
    
    * nix fmt
    
    ---------
    
    Co-authored-by: Blake Haug <blake@blakehaug.com>

--- hosts/servers/koi.nix
+++ hosts/servers/koi.nix
@@ -11,7 +11,7 @@
   networking.hostName = "koi";
 
   ocf.motd.description = ''
-    Welcome to the new NixOS based staff login server!
+    Welcome to the new NixOS based staff login and development server!
       - install a package: nix profile add 'nixpkgs#package-name'
       - upgrade all packages: nix profile upgrade --all
       - ...or manage packages declaratively with home-manager!
@@ -38,6 +38,51 @@
     owner = "root";
     group = "ocfstaff";
     mode = "0640";
+  };
+
+  # like puppet certs, needed for the ocfmail-dev and ocfstats-dev users that are in the ocfweb tests suite, which is run on koi.
+  age.secrets.ocfweb-conf = {
+    rekeyFile = ../../secrets/master-keyed/koi/ocfweb.conf.age;
+    path = "/etc/ocfweb/ocfweb.conf";
+    owner = "root";
+    group = "ocfstaff";
+    mode = "0640";
+  };
+
+  # These puppet certs are necessary to (at least) load the servers page properly in the dev environment on ocfweb. The puppetdb must be queried by koi when running ocfweb tests to get correct information about puppet hosts (ex. which ones are kvm hypervisors, what are their corresponding VMs)
+  age.secrets.puppet-ca = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-ca.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-ca.pem";
+    owner = "root";
+    mode = "0644";
+  };
+
+  age.secrets.puppet-cert = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-cert.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-cert.pem";
+    owner = "root";
+    mode = "0644";
+  };
+
+  age.secrets.puppet-private = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-private.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-private.pem";
+    owner = "root";
+    mode = "0644";
+  };
+
+  age.secrets.puppet-public = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-public.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-public.pem";
+    owner = "root";
+    mode = "0644";
+  };
+
+  age.secrets.puppet-signed = {
+    rekeyFile = ../../secrets/master-keyed/koi/puppet-signed.pem.age;
+    path = "/etc/ocfweb/puppet-certs/puppet-signed.pem";
+    owner = "root";
+    mode = "0644";
   };
 
   system.stateVersion = "25.05";
