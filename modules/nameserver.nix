5d438877541185dfc8d493b62d5c025db55ec496
Robert Schütz <ocf@dotlambda.de>
2026-07-31 20:27:11 -0700

    use systemctl reload-or-restart

--- modules/nameserver.nix
+++ modules/nameserver.nix
@@ -7,6 +7,9 @@
 }:
 
 let
+  bindGroup = config.users.users.${bindUser}.group;
+  bindUser = config.systemd.services.bind.serviceConfig.User;
+
   build-zones = pkgs.stdenv.mkDerivation {
     name = "build-zones";
     src = inputs.ocf-dns;
@@ -32,10 +35,8 @@ in
   config = lib.mkIf cfg.enable {
     services.bind = {
       enable = true;
+      # TODO this should actually be https://github.com/ocf/puppet/blob/master/modules/ocf_ns/templates/named.conf.options.erb
       configFile = "/srv/dns/etc/named.conf.local";
-    };
-    systemd.services.bind = {
-      after = [ "rebuild-dns-from-ldap.service" ];
     };
 
     systemd.services.rebuild-dns-from-ldap = {
@@ -46,7 +47,8 @@ in
       ];
       script = ''
         cd "$(mktemp -d)"
-        cp -r ${inputs.ocf-dns}/. .
+        cp --no-preserve=mode -r ${inputs.ocf-dns}/{etc,templates} .
+        sed -i /auto-dnssec/d etc/named.conf.local  # removed in bind 9.19.16
         build-zones
         check-zones
         cp -r etc /run/dns/
@@ -58,8 +60,9 @@ in
           "+${lib.getExe' pkgs.coreutils "mkdir"} -p /srv/dns/"
           # cannot use atomic `exch` for the next two operations because /run/dns is on a different fs (bind mount)
           "+${lib.getExe' pkgs.coreutils "rm"} -rf /srv/dns/etc"
-          "+${lib.getExe' pkgs.coreutils "mv"} /run/dns/etc /srv/dns/"
-          "+${lib.getExe' pkgs.systemd "systemctl"} reload bind.service"
+          "+${lib.getExe' pkgs.coreutils "cp"} --no-preserve=ownership -r /run/dns/etc /srv/dns/"
+          "+${lib.getExe' pkgs.coreutils "chown"} -R ${bindUser}:${bindGroup} /srv/dns"
+          "+${lib.getExe' pkgs.systemd "systemctl"} reload-or-restart bind.service"
         ];
         RuntimeDirectory = "dns";
         Type = "oneshot";
