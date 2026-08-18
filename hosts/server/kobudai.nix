cdbc6326e387a49bca706bc8ea9e0485dd6d6044
Oliver Ni <oliverni@ocf.berkeley.edu>
2026-08-17 20:10:26 -0700

    Revise comments on NFS delegations and fixes
    Updated comments regarding NFS write delegations and linked relevant issues.

--- hosts/server/kobudai.nix
+++ hosts/server/kobudai.nix
@@ -19,10 +19,11 @@
   # in linux 6.6, nfsd added write delegations. however, this causes old clients to
   # get stuck spamming TEST_STATEID (~5000/s) causing open() to take multiple seconds:
   #
-  # https://gitlab.com/gitlab-org/gitlab-foss/-/work_items/52017
-  #
   # fixed client-side in linux 6.7, but death (and others) are super old (5.10 lol),
   # so let's disable delegations for now by disabling vfs leases.
+  #
+  # issue: https://gitlab.com/gitlab-org/gitlab-foss/-/work_items/52017
+  # fix: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=a9b8d90f8726
   boot.kernel.sysctl."fs.leases-enable" = 0;
 
   services.nfs.server = {
