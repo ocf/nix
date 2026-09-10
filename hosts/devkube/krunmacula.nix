6ebfc125f57c9482c71a66c37f94acc5bc9c4c34
sophiebsw <sophiebsw@gmail.com>
2026-09-10 05:56:51 -0700

    nix fmt

--- hosts/devkube/krunmacula.nix
+++ hosts/devkube/krunmacula.nix
@@ -27,7 +27,6 @@
     staging = true;
   };
 
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
