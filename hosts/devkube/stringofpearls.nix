90fab25e2b8b5f5d24f2d37ff6b04362a10a6a9c
sophiebsw <sophiebsw@gmail.com>
2026-09-10 00:16:29 -0700

    nix fmt

--- hosts/devkube/stringofpearls.nix
+++ hosts/devkube/stringofpearls.nix
@@ -10,8 +10,14 @@
     lastOctet = 102;
     extraRoutes = [
       # We use these subnets for Kubernetes, they aren't part of the main /64
-      { Destination = "2607:f140:8801:1::/64"; Scope = "link"; }
-      { Destination = "2607:f140:8801:2::/64"; Scope = "link"; }
+      {
+        Destination = "2607:f140:8801:1::/64";
+        Scope = "link";
+      }
+      {
+        Destination = "2607:f140:8801:2::/64";
+        Scope = "link";
+      }
     ];
   };
 
