df75436d3c134b24fed347df502b191a83f6d179
sophiebsw <sophiebsw@gmail.com>
2026-09-16 00:28:36 -0700

    disko config

--- hosts/server/vaccine.nix
+++ hosts/server/vaccine.nix
@@ -15,6 +15,38 @@
     lastOctet = 14;
   };
 
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/disk/by-id/ata-SuperMicro_SSD_SMC0515D92517CE71175";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
   nixpkgs.hostPlatform = "x86_64-linux";
 
   system.stateVersion = "26.05";
