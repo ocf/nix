bdeeb2e2b29f72196f0e19afbec7cb8efa0b3ccc
kafu-densha <git@kafu.observer>
2026-09-17 15:03:16 -0700

    printhost: simplify and move email config
    - use config.networking.domain in email config
    - move to printhost/default.nix to prepare for dev-printhost

--- modules/printhost/default.nix
+++ modules/printhost/default.nix
@@ -36,6 +36,19 @@
         "/var/lib/acme/${config.networking.fqdn}/key.pem";
     };
 
+    # Postfix relay so ocflib can send mail via sendmail.
+    services.postfix = {
+      enable = true;
+      settings.main = {
+        mydomain = config.networking.domain;
+        myorigin = config.networking.domain;
+        mydestination = "";
+        inet_interfaces = "loopback-only";
+        relayhost = [ "smtp.${config.networking.domain}" ];
+        sender_canonical_maps = "static:root@${config.networking.domain}";
+      };
+    };
+
     # add all CNAMEs to tule's cert
     ocf.acme.extraCerts = [
       "printhost.ocf.berkeley.edu"
